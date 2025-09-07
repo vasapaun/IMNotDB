8168fc44a7d43470a2892280aa7d559b66701bd2
vasapaun <vasapaunovic674@gmail.com>
2025-09-07 22:50:37 +0200

    Rerouted

--- routes/api.php
+++ routes/api.php
@@ -11,6 +11,6 @@ Route::post('/login', [AuthController::class, 'login'])->name('api.login');
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::post('/logout', [AuthController::class, 'logout'])->name('api.logout');
-    Route::get('/films/search', [FilmController::class, 'search'])->name('films.search');
+    Route::get('/films/search', [FilmController::class, 'search'])->name('api.films.search');
     Route::apiResource('/films', FilmController::class);
 });
