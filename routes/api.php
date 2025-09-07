07f6ed2046e607dfe070859cc35041051b47bd72
vasapaun <vasapaunovic674@gmail.com>
2025-09-07 22:42:33 +0200

    Rerouted

--- routes/api.php
+++ routes/api.php
@@ -6,11 +6,11 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\FilmController;
 use App\Http\Controllers\AuthController;
 
-Route::post('/register', [AuthController::class, 'register'])->name('register');
-Route::post('/login', [AuthController::class, 'login'])->name('login');
+Route::post('/register', [AuthController::class, 'register'])->name('api.register');
+Route::post('/login', [AuthController::class, 'login'])->name('api.login');
 
 Route::middleware('auth:sanctum')->group(function () {
-    Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+    Route::post('/logout', [AuthController::class, 'logout'])->name('api.logout');
     Route::get('/films/search', [FilmController::class, 'search'])->name('films.search');
     Route::apiResource('/films', FilmController::class);
 });
